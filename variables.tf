d0cb145b17050586f19601c14853343f940c6342
Tim Sharpe <tim@bombasticmonkey.com>
2019-06-19 17:25:08 +1000

    initial commit

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "pe_version" {
+  default = "2018.1.2"
+}
+
+variable "pe_platform" {
+  default = "ubuntu-16.04-amd64"
+}
+
+variable "access_key" {
+}
+
+variable "secret_key" {
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "aws_key_pair" {
+}
+
+variable "aws_ami_id" {
+}
+
